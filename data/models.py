bc5531f80ea1bea9e10ef549203965c8733023b2
blessed <ytminicraftyt@gmail.com>
2024-12-27 18:00:47 +0200

    tourupd3

--- data/models.py
+++ data/models.py
@@ -1,5 +1,8 @@
-from sqlalchemy import String, ForeignKey
+from typing import List
+
+from sqlalchemy import String, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from flask_login import UserMixin
 
 from data.base import Base
 
@@ -8,23 +11,30 @@ class Tour(Base):
     __tablename__ = "tours"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    tittle: Mapped[str] = mapped_column(String(100))
-    description: Mapped[str] = mapped_column(String(200))
-    departure: Mapped[str] = mapped_column(String(100))
+    title: Mapped[str] = mapped_column(String(50))
+    description: Mapped[str] = mapped_column(String(100))
+    departure: Mapped[str] = mapped_column(String(50))
     picture: Mapped[str] = mapped_column(String(500))
     price: Mapped[int] = mapped_column()
-    stars: Mapped[str] = mapped_column(String(5))
-    country: Mapped[str] = mapped_column(String(100))
-    nights: Mapped[int] = mapped_column()
-    date: Mapped[str] = mapped_column(String(100))
+    stars: Mapped[str] = mapped_column(String(10))
+    country: Mapped[str] = mapped_column(String(50))
+    nights: Mapped[str] = mapped_column(String(10))
+    date: Mapped[str] = mapped_column(String(50))
 
 
-class Reserve(Base):
-    __tablename__ = "reserve"
+tour_user_assoc = Table(
+    "tour_user_assoc",
+    Base.metadata,
+    Column("tour_id", ForeignKey("tours.id"), primary_key=True),
+    Column("user_id", ForeignKey("users.id"), primary_key=True)
+)
+
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(255))
-    tour_id: Mapped[int] = mapped_column(ForeignKey(Tour.id))
-    tour: Mapped[Tour] = relationship()
-
-
+    username: Mapped[str] = mapped_column(String(100), unique=True)
+    email: Mapped[str] = mapped_column(String(500), unique=True)
+    password: Mapped[str] = mapped_column(String(500))
+    tours: Mapped[List[Tour]] = relationship(secondary=tour_user_assoc)
